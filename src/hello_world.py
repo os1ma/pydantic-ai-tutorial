e68e89aea08a040e1d7c1c61672fef6548c9ebfe
Yuki Oshima <39944763+os1ma@users.noreply.github.com>
2024-12-24 04:16:08 +0000

    Add samples

---
+++ src/hello_world.py
@@ -0,0 +1,15 @@
+from dotenv import load_dotenv
+from pydantic_ai import Agent
+
+load_dotenv(override=True)
+
+agent = Agent(
+    "openai:gpt-4o",
+    system_prompt="Be concise, reply with one sentence.",
+)
+
+result = agent.run_sync('Where does "hello world" come from?')
+print(result.data)
+"""
+The first known use of "hello, world" was in a 1974 textbook about the C programming language.
+"""
